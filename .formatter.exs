7f8e2822d70858c9f1d89dec300f7657a72890b7
Oskar Niburski <oskarniburski@gmail.com>
2019-11-22 00:00:19 -0800

    feat(docs): Add base elixir file

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,4 @@
+# Used by "mix format"
 [
-  import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: []
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
---
+++ lib/algorithims.ex
@@ -0,0 +1,18 @@
+defmodule Algorithims do
+  @moduledoc """
+  Documentation for Algorithims.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Algorithims.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
