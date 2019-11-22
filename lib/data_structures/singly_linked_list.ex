859bdb402048c06ed421f39ab2b2e9d27a9483c8
Oskar Niburski <oskarniburski@gmail.com>
2019-11-22 01:00:06 -0800

    update(travis): update for recent elixir v going forward

--- lib/data_structures/singly_linked_list.ex
+++ lib/data_structures/singly_linked_list.ex
@@ -1,15 +1,15 @@
-defmodule Algorithims.DataStructures.LinkedList do
+defmodule Algorithims.DataStructures.SinglyLinkedList do
   defmodule SingleLink, do: defstruct([:value, :reference])
 
   def add_node([], value) do
-    [form_singly_linked_list(value, nil)]
+    [form_link(value, nil)]
   end
 
   def add_node([head | _tail] = list, value) do
-    [form_singly_linked_list(value, head) | list]
+    [form_link(value, head) | list]
   end
 
-  defp form_singly_linked_list(value, reference) do
+  defp form_link(value, reference) do
     %__MODULE__.SingleLink{value: value, reference: reference}
   end
 end
