bed2674041bced11cbbdf6a437429fe0846ba00c
Frank Schmitt <frankschmitt@gmx.de>
2021-10-14 22:22:14 +0200

    add dynamic programming algo for Fibonacci numbers

---
+++ lib/dynamic_programming/fibonacci.ex
@@ -0,0 +1,34 @@
+defmodule Algorithms.DynamicProgramming.Fibonacci do
+  @moduledoc """
+  Implementation of the Fibonacci sequence using Dynamic Programming / Memoization.
+  """
+  require Integer
+  require Map
+
+  @doc """
+    Given an index i, returns the i-th Fibonacci number
+  """
+  @spec fibonacci(Integer) :: Integer
+  def fibonacci(0), do: 0
+  def fibonacci(1), do: 1
+  def fibonacci(n) do 
+    {val, _} = memoize(n, %{0 => 0, 1 => 1})
+    val
+  end
+
+  @doc """
+    Given an index n and a map of known Fibonacci numbers, returns the n-th Fibonacci
+    number and a map containing all i -> Fib_i pairs up to (including) n
+  """
+  @spec memoize(Integer, map) :: {Integer, map}
+  def memoize(n, memo) do
+    case Map.has_key?(memo, n) do
+      true -> {Map.get(memo, n), memo}
+      false -> {val2, m2} = memoize(n-2, memo)
+               {val1, m1} = memoize(n-1, m2)
+               val = val1 + val2  
+               memoize(n, Map.merge(m1, %{ n => val}))
+    end 
+  end
+    
+end
---
+++ test/dynamic_programming/fibonacci_test.exs
@@ -0,0 +1,29 @@
+defmodule Algorithms.DynamicProgramming.FibonacciTest do
+  alias Algorithms.DynamicProgramming.Fibonacci
+
+  use ExUnit.Case
+  doctest Algorithms
+
+  describe "fibonacci/1 - example test cases" do
+    test "fib 1" do
+      assert Fibonacci.fibonacci(1) == 1
+    end
+
+    test "fib 2" do
+      assert Fibonacci.fibonacci(2) == 1
+    end
+
+    test "fib 3" do
+      assert Fibonacci.fibonacci(3) == 2
+    end
+
+    test "fib 5" do
+      assert Fibonacci.fibonacci(5) == 5
+    end
+
+    test "fib 10" do
+      assert Fibonacci.fibonacci(10) == 55
+    end
+  end
+
+end
